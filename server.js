a23031be10715d0b53b6c921a0a3a2e1ba89dfe4
Youngmi Lee <abinding@gmail.com>
2018-07-07 12:45:30 -0500

    added passport js files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -15,8 +16,30 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
+// For Passport
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+app.get("/", function(req, res) {
+  res.send("Welcome to Passport with Sequelize");
+});
+
+//Models
+var models = require("./models");
+
+//Routes
+var authRoute = require("./routes/auth.js")(app, passport);
+
+//load passport strategies
+require("./config/passport/passport.js")(passport, models.user);
+
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/happykids"
+);
 
 // Start the API server
 app.listen(PORT, function() {
