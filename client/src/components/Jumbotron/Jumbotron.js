c5938a02eae22931e930583f88cf2d312f113f12
Pamela Pelkola <pelkola.pam@gmail.com>
2018-07-14 12:44:44 -0500

    updated Jumbotron, Carousel styling added text

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const Jumbotron = ({ children }) => (
   <div
-    style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
+    style={{ height: 300, clear: "both", paddingTop: 10, textAlign: "center", marginTop:0, marginLeft:120, marginRight: 120}}
     className="jumbotron"
   >
+  <h1 className="display-1">Welcome to Happy Kids Accessories!</h1>
+        <p className="lead">Personalized Wooden Toys</p>
+       
     {children}
   </div>
 );
