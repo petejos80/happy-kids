2ac0eb78e4424348779fa824373e3055c34ef934
Pamela Pelkola <pelkola.pam@gmail.com>
2018-07-15 07:12:09 -0500

    updated jumbotron and home page

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,15 +1,15 @@
 import React from "react";
+import { Jumbotron } from 'reactstrap';
 
-const Jumbotron = ({ children }) => (
+const Banner = ({ children }) => (
   <div
     style={{ height: 300, clear: "both", paddingTop: 10, textAlign: "center", marginTop:0, marginLeft:120, marginRight: 120}}
     className="jumbotron"
   >
-  <h1 className="display-1">Welcome to Happy Kids Accessories!</h1>
-        <p className="lead">Personalized Wooden Toys</p>
+
        
     {children}
   </div>
 );
 
-export default Jumbotron;
+export default Banner;
