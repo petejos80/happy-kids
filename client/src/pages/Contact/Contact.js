cbb5f9421d3c59a2230c4d52b6ac042f61b572f5
Pamela Pelkola <pelkola.pam@gmail.com>
2018-07-17 19:28:32 -0500

    form

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -4,10 +4,11 @@ import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 export default class Contact extends React.Component {
   render() {
     return (
-      <Form method="POST" action="https://formspree.io/YOUREMAILHERE">
+      <div>
+      <Form className="form" method="POST" action="https://formspree.io/pelkola.pam@gmail.com">
       <h1>Contact Us</h1>
       <FormGroup className="form-one">
-  <Input type="email" name="email" placeholder="Your email" />
+  <Input className="form" type="email" name="email" placeholder="Your email" />
   <Label name="message" placeholder="Your message" />
   </FormGroup>
   <FormGroup className="form-two">
@@ -17,32 +18,14 @@ export default class Contact extends React.Component {
   <FormGroup className="form-two">
   <Input type="address" name="address" placeholder="Your Address" />
   <Label name="message" placeholder="Your message" />
+  <Button>Submit</Button>
   </FormGroup>
-  <Button type="submit">Send</Button>
-</Form>
-      //  <Form>
-      //   <FormGroup className="form-one">
-      //     <Label for="exampleEmail">Email</Label>
-      //     <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-      //   </FormGroup>
-      //   <FormGroup className="form-two">
-      //     <Label for="exampleName">Name</Label>
-      //     <Input type="Name" name="Username" id="exampleName" placeholder="name placeholder" />
-      //   </FormGroup>
-      //   <FormGroup className="form-three">
-      //     <Label for="exampleAddress">Address</Label>
-      //     <Input type="Address" name="Address" id="exampleAddress" placeholder="Address placeholder" />
-      //   </FormGroup>
-       
-      //   <FormGroup className="form-four">
-      //     <Label for="exampleText">Text Area</Label>
-      //     <Input type="textarea" name="text" id="exampleText" />
-      //   </FormGroup>
-        
-       
+  
+
          
       //   <Button>Submit</Button>
       // </Form> 
+      </div>
     );
   }
 }
