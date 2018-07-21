83fe188cd82f4fb95e60644952031453fc5a913b
petejos80 <petejos80@gmail.com>
2018-07-21 13:11:03 -0500

    Working authorization

--- routes/api/item.js
+++ routes/api/item.js
@@ -6,30 +6,30 @@ var passport = require('passport');
 require('../../config/passport')(passport);
 
 /* GET ALL ITEMS */
-// router.get('/', passport.authenticate('jwt', { session: false}), function(req, res) {
-  router.get('/', function(req, res) {
-  // var token = getToken(req.headers);
-  // if (token) {
+router.get('/', passport.authenticate('jwt', { session: false}), function(req, res) {
+  // router.get('/', function(req, res) {
+  var token = getToken(req.headers);
+  if (token) {
     Item.find(function (err, items) {
       if (err) return next(err);
       res.json(items);
     });
-  // } else {
-    // return res.status(403).send({success: false, msg: 'Unauthorized.'});
-  // }
+  } else {
+    return res.status(403).send({success: false, msg: 'Unauthorized.'});
+  }
 });
 
-// router.post('/', passport.authenticate('jwt', { session: false}), function(req, res) {
-router.post('/', function(req, res) {
-  // var token = getToken(req.headers);
-  // if (token) {
+router.post('/', passport.authenticate('jwt', { session: false}), function(req, res) {
+// router.post('/', function(req, res) {
+  var token = getToken(req.headers);
+  if (token) {
     Item.create(req.body, function (err, post) {
       if (err) return next(err);
       res.json(post);
     });
-  // } else {
-    // return res.status(403).send({success: false, msg: 'Unauthorized.'});
-  // }
+  } else {
+    return res.status(403).send({success: false, msg: 'Unauthorized.'});
+  }
 });
 
 getToken = function (headers) {
