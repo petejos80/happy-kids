faa61aa60f737097e163c276de6f73e70c2e7ef8
@Donskoy <diman@rocketmail.com>
2018-07-26 13:25:35 -0500

    dbseed pacifierholders

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,32 +1,57 @@
 const mongoose = require("mongoose");
 const db = require("../models");
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
 // This file empties the Books collection and inserts the books below
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/happykids",
-  {
-    useMongoClient: true
-  }
+  // { useNewUrlParser: true }
 );
 
 const itemSeed = [
   {
-    name: "beads 1",
-    image: "www.image.com",
-    price: "111",
-    category: "Beads",
-    description: "Beads galore",
+    name: "PACIFIER HOLDER",
+    image: "https://scontent.ffcm1-1.fna.fbcdn.net/v/t1.0-9/28055832_324404991381078_8035239833306901251_n.jpg?_nc_cat=0&oh=e7d1fb602937a82721b986a8a1915104&oe=5BCB4663",
+    price: "$12",
+    category: "Pacifierholders",
+    description: "✅You will never loose the pacifier again!",
     date: new Date(Date.now())
   },
   
   {
-    name: "beads 222",
-    image: "www.image222.com",
-    price: "222",
-    category: "Beads",
-    description: "Beads galore",
+    name: "",
+    image: "",
+    price: "",
+    category: "",
+    description: "",
+    date: new Date(Date.now())
+  },
+
+  {
+    name: "",
+    image: "",
+    price: "",
+    category: "",
+    description: "",
+    date: new Date(Date.now())
+  },
+
+  {
+    name: "",
+    image: "",
+    price: "",
+    category: "",
+    description: "",
+    date: new Date(Date.now())
+  },
+
+  {
+    name: "",
+    image: "",
+    price: "",
+    category: "",
+    description: "",
     date: new Date(Date.now())
   },
 
@@ -37,7 +62,7 @@ db.Item
   .remove({})
   .then(() => db.Item.collection.insertMany(itemSeed))
   .then(data => {
-    console.log(data.insertedIds.length + " records inserted!");
+    console.log(data.result.n + " records inserted!");
     process.exit(0);
   })
   .catch(err => {
